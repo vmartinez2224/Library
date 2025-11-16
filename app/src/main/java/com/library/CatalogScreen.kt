d5289ba5d2103a89c5ca71c30d5877a16b3ab495
Valeria <vmartinez2224@cue.edu.co>
2025-11-15 19:54:39 -0500

    Imports

--- app/src/main/java/com/library/CatalogScreen.kt
+++ app/src/main/java/com/library/CatalogScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
